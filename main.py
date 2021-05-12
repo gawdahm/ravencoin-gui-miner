22df8356af6eb7bdd2af1223844d62aa1d49e7ce
gawdahm <styvwheeler@gmail.com>
2021-05-12 12:11:58 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,148 @@
+from tkinter import *
+import subprocess, os, requests, pickle, threading,  time
+from sys import platform
+from tkinter.filedialog import askopenfilename
+
+PROCPATH = "./"
+PROCNAME = "xmrig"
+balance = "0"
+hashrate = "0"
+
+
+mining_pools = {
+    'Ravenpool' : { 'url' : 'https://www.ravenminer.com/api/wallet?address=', 'balance_key' : 'balance'},
+    '2miners' : { 'url' : 'https://btg.2miners.com/api/accounts/', 'balance_key' : 'balance'},
+    'Test' : { 'url' : 'https://btg.2miners.com/api/accounts/', 'balance_key' : 'balance'},
+}
+
+def getminingdata():
+    # Ravenminer and 2miners have an open api and don't require registration
+    headers = {
+        'accept': 'application/json',
+    }
+
+    print(requests.get(mining_pools[miningpoolname.get()]['url'] + walletaddr.get(), headers=headers).content)
+
+def populate_mining_pool_dropdown():
+    pools = []
+    for key in mining_pools:
+        pools.append(key)
+    return pools
+
+# Button functions
+def change_dropdown(*args):
+    print( miningpoolname.get() )
+
+def get_wallet_addr(*args):
+    print( walletaddr.get() )
+
+def save_settings():
+    a_file = open("data.pkl", "wb")
+    dictionary_data = {
+        'wallet' : walletaddr.get(), 
+        'miningpoolname' : miningpoolname.get(),
+        'minerpath' : minerpath.get(),
+        }
+    pickle.dump(dictionary_data, a_file)
+    a_file.close()
+
+def check_address():
+    if not walletaddr.get():
+        return False
+    return True
+
+def start_mining():
+    if not check_address():
+        print("Invalid wallet")
+        return
+    #getminingdata(  )
+    p = subprocess.Popen(minerpath.get() + PROCNAME + " --url=cryptonote.social:2222 --user {} --threads=1 --pass='email=runforestrun@airmail.cc'".format(walletaddr.get()), shell=True)
+    print("Mining started!")
+
+def stop_mining():
+    if platform == "linux" or platform == "linux2" or platform == "darwin":
+        os.system('pkill '+PROCNAME)
+    elif platform == "win32":
+        os.system("taskkill /f /im {}.exe".format(PROCNAME))
+
+root = Tk()
+
+# Menu
+def NewFile():
+    print("New File!")
+def OpenFile():
+    name = askopenfilename()
+    print(name)
+def About():
+    print("This is a simple example of a menu")
+
+menu = Menu(root)
+root.config(menu=menu)
+filemenu = Menu(menu)
+menu.add_cascade(label="File", menu=filemenu)
+filemenu.add_command(label="New", command=NewFile)
+filemenu.add_command(label="Open...", command=OpenFile)
+filemenu.add_separator()
+filemenu.add_command(label="Exit", command=root.quit)
+
+helpmenu = Menu(menu)
+menu.add_cascade(label="Help", menu=helpmenu)
+helpmenu.add_command(label="About...", command=About)
+# !Menu
+
+# Add a grid
+mainframe = Frame(root)
+mainframe.grid(column=0,row=0, sticky=(N,W,E,S) )
+mainframe.columnconfigure(0, weight = 1)
+mainframe.rowconfigure(0, weight = 1)
+mainframe.pack(pady = 100, padx = 100)
+
+# Create a Tkinter variable
+miningpoolname = StringVar(root)
+walletaddr = StringVar(root)
+minerpath = StringVar(root)
+
+# Miner Path
+Label(mainframe, text="Path to executable: ").grid(row = 1, column = 1)
+miner_path_entry = Entry(mainframe, textvariable=minerpath).grid(row = 1, column = 2)
+# Wallet Info
+Label(mainframe, text="Wallet Address: ").grid(row = 2, column = 1)
+wallet_entry = Entry(mainframe, textvariable=walletaddr).grid(row = 2, column = 2)
+# Mining pool drop down
+choices = populate_mining_pool_dropdown()
+popupMenu = OptionMenu(mainframe, miningpoolname, *choices)
+Label(mainframe, text="Mining Pool: ").grid(row = 3, column = 1)
+popupMenu.grid(row = 3, column = 2)
+Label(mainframe, text="Hashrate ").grid(row = 4, column = 1)
+Label(mainframe, text=balance).grid(row = 4, column = 2)
+Label(mainframe, text="Balance ").grid(row = 5, column = 1)
+Label(mainframe, text=hashrate).grid(row = 5, column = 2)
+
+#  track changes
+miningpoolname.trace('w', change_dropdown)
+walletaddr.trace("w", get_wallet_addr)
+
+start_mining_btn = Button(mainframe, text = "Start Mining", command = lambda: start_mining()).grid(row = 6, column = 1)
+stop_mining_btn = Button(mainframe, text = "Stop Mining", command = lambda: stop_mining()).grid(row = 6, column = 2)
+save_settings_btn = Button(mainframe, text = "Save", command = lambda: save_settings()).grid(row = 7, column = 1)
+
+
+# Load settings
+# init file if it doesn't exist and populate it with blank entries
+# TODO: Inefficient
+if not os.path.isfile("data.pkl"):
+    f = open("data.pkl", "w")
+    f.close()
+    save_settings()
+
+
+a_file = open("data.pkl", "rb")
+output = pickle.load(a_file)
+a_file.close()
+
+# Populate fields
+walletaddr.set(output['wallet'])
+minerpath.set(output['minerpath']) # set the default option
+miningpoolname.set(output['miningpoolname']) # set the default option
+
+mainloop()
